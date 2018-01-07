b2eaec7663b2705034a8a5a986cc27ff01ca8a6f
Michael Schmidt <michael.j.schmidt@gmail.com>
2018-01-07 22:34:48 +0100

    inits dstm zcash miner

--- coreos-nvidia-installer/install.sh
+++ coreos-nvidia-installer/install.sh
@@ -4,26 +4,45 @@ set -x
 
 rm /opt/nvidia/current
 ln -fs /opt/nvidia/$DRIVER_VERSION/$COREOS_VERSION/bin/* /opt/bin
-ln -fs /opt/nvidia/$DRIVER_VERSION/$COREOS_VERSION /opt/nvidia/current
-ln -fs /opt/nvidia/$DRIVER_VERSION/$COREOS_VERSION/lib64/libnvidia-ml.so /opt/nvidia/$DRIVER_VERSION/$COREOS_VERSION/lib64/libnvidia-ml.so.$DRIVER_VERSION
+ln -fs $DRIVER_VERSION/$COREOS_VERSION /opt/nvidia/current
+ln -fs libnvidia-ml.so.$NVIDIA_DRIVER_VERSION /opt/nvidia/$NVIDIA_DRIVER_VERSION/$COREOS_VERSION/lib64/libnvidia-ml.so
 rm /opt/nvidia/$DRIVER_VERSION/$COREOS_VERSION/lib64/libEGL.so.$DRIVER_VERSION
 chmod u+s /opt/nvidia/$DRIVER_VERSION/$COREOS_VERSION/bin/nvidia-modprobe
 
+cat <<EOF > /etc/systemd/system/nvidia-update.service
+[Unit]
+After=docker.service
+Requires=docker.service
+Description=NVIDIA Update Driver
+
+[Service]
+EnvironmentFile=/etc/os-release
+TimeoutStartSec=0
+Type=oneshot
+RemainAfterExit=yes
+ExecStartPre=/usr/bin/docker pull bugroger/coreos-nvidia-installer:\${VERSION}-$DRIVER_VERSION
+ExecStart=/usr/bin/docker run -v /:/rootfs --privileged bugroger/coreos-nvidia-installer:\${VERSION}-$DRIVER_VERSION
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 cat <<EOF > /etc/systemd/system/usr-lib64.mount
 [Unit]
-Description=Nvidia Kernel Modules
-Before=local-fs.target
-ConditionPathExists=/opt/nvidia/$DRIVER_VERSION/$COREOS_VERSION/lib64
+After=nvidia-update.service
+Requires=nvidia-update.service
 ConditionPathExists=/opt/nvidia/.work
+Description=Nvidia Kernel Modules
 
 [Mount]
+EnvironmentFile=/etc/os-release
 Type=overlay
 What=overlay
 Where=/usr/lib64
-Options=lowerdir=/usr/lib64,upperdir=/opt/nvidia/$DRIVER_VERSION/$COREOS_VERSION/lib64,workdir=/opt/nvidia/.work
+Options=lowerdir=/usr/lib64,upperdir=/opt/nvidia/$DRIVER_VERSION/\${VERSION}/lib64,workdir=/opt/nvidia/.work
 
 [Install]
-WantedBy=local-fs.target
+WantedBy=multi-user.target
 EOF
 
 cat <<EOF > /etc/systemd/system/nvidia-persistenced.service
@@ -59,26 +78,6 @@ ExecStart=/opt/bin/nvidia-smi
 WantedBy=multi-user.target
 EOF
 
-cat <<EOF > /etc/systemd/system/nvidia-update.service
-[Unit]
-After=docker.service
-Requires=docker.service
-ConditionPathExists=!/opt/nvidia/$DRIVER_VERSION/$COREOS_VERSION
-Description=NVIDIA Update Driver
-
-[Service]
-EnvironmentFile=/etc/coreos/update.conf
-TimeoutStartSec=0
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/usr/bin/docker pull bugroger/coreos-nvidia-driver:stable
-ExecStart=/usr/bin/docker run -v /:/rootfs --privileged bugroger/coreos-nvidia-driver:$GROUP
-ExecStart=/usr/sbin/reboot
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
 cat <<EOF > /etc/udev/rules.d/01-nvidia.rules
 SUBSYSTEM=="pci", ATTRS{vendor}=="0x10de", DRIVERS=="nvidia", TAG+="seat", TAG+="master-of-seat"
 EOF
@@ -87,12 +86,7 @@ udevadm control --reload-rules
 useradd -c "NVIDIA Persistence Daemon" --shell /sbin/nologin --home-dir / nvidia-persistenced
 
 systemctl daemon-reload
+systemctl enable nvidia-update
 systemctl enable usr-lib64.mount
-systemctl start usr-lib64.mount
-
 systemctl enable nvidia
-systemctl start nvidia
-
 systemctl enable nvidia-persistenced
-systemctl start nvidia-persistenced
-
